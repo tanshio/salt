4e73c355479c7e5204dfff142067e6bce92174b7
tanshio <rakuburomail+tanshio@gmail.com>
2017-07-21 23:18:29 +0900

    Add three.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
 import Link from 'next/link'
-export default () => (
-  <div>
-    <div>Index to next.js!</div>
-    <div>Click <Link href="/about"><a>here</a></Link> to read more</div>
-  </div>
-)
+// export default () => (
+//   <div>
+//     <div>Index to next.js!</div>
+//     <div>Click <Link href="/about"><a>here</a></Link> to read more</div>
+//   </div>
+// )
+// import React from 'react'
+export default class extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      aaa: 'test'
+    };
+
+  }
+  getInitialState() {
+    return {
+      aaa: "aaa"
+    }
+  }
+  static async getInitialProps ({ req }) {
+    return req
+      ? { userAgent: req.headers['user-agent'] }
+      : { userAgent: navigator.userAgent }
+  }
+  render () {
+    return <div>
+      Hello World {this.props.userAgent}, {this.state.aaa}
+      <div>Click <Link href="/about"><a>here</a></Link> to read more</div>
+    </div>
+  }
+}
