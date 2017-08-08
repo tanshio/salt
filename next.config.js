48453338363e347a8b4a2e1470b5276d4ee5df14
tanshio <rakuburomail+tanshio@gmail.com>
2017-08-09 04:28:43 +0900

    Add SVG

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   exportPathMap: function () {
     return {
       "/": { page: "/" },
-      "/about": { page: "/about" }
+      "/about": { page: "/about" },
+      "/site": { page: "/site" },
       // "/p/hello-nextjs": { page: "/post", query: { title: "hello-nextjs" } },
       // "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } },
       // "/p/deploy-nextjs": { page: "/post", query: { title: "deploy-nextjs" } }
